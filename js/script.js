9b6d62810317cbe190267325308bf6fd97645099
Tyler Fisher <tyler@tylerjfisher.com>
2013-12-26 14:25:02 -0600

    fix for slider nav

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
-	$('.bxslider').bxSlider({
+	var slider = $('.bxslider').bxSlider({
 		pager: false,
 		adaptiveHeight: true
 	});
 
 	$('#snowden-nav').on('click',function(){
-		$('.bxslider').goToSlide(1);
+		slider.goToSlide(1);
 	});
+
+    $('#pope-nav').on('click',function(){
+        slider.goToSlide(2);
+    });
 
 });
